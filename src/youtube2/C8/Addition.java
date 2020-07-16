f85e05789530b42c927cb06adb8adb41db28fcd9
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2020-07-16 22:26:44 +0300

    tr1

--- src/youtube2/C8/Addition.java
+++ src/youtube2/C8/Addition.java
@@ -1,4 +1,10 @@
 package youtube2.C8;
 
 public class Addition {
+    public static void main(String[] args) {
+        System.out.println( add(5,6));
+    }
+    public static int add(int no1, int no2){
+        return no1 + no2;
+    }
 }
